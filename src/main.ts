d98528e3010d241d8af3826a34890abad54ca3ce
Diwwy20 <123185597+Diwwy20@users.noreply.github.com>
2025-11-03 02:56:47 +0700

    chore: add origin method credential in cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL || 'https://vote-quote.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', 
+    credentials: true, 
+  });
   
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
