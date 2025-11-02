acf25f639e1af8660aa0f9702af1411b01172b5f
Diwwy20 <123185597+Diwwy20@users.noreply.github.com>
2025-11-03 03:41:30 +0700

    commit: add structure import

--- src/utils/dataUri.ts
+++ src/utils/dataUri.ts
@@ -1,8 +1,8 @@
-import parser from 'datauri/parser';
+import { DataUriParser } from 'datauri/parser';
 import * as path from 'path';
 
 export const getBuffer = (file: Express.Multer.File) => {
-  const dataUri = new parser();
+  const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
-  return dataUri.format(extName, file.buffer);
+  return parser.format(extName, file.buffer);
 };
