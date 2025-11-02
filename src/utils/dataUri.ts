b6a7cacfc00ce7a34561ddca050a9590825aaeb2
Diwwy20 <123185597+Diwwy20@users.noreply.github.com>
2025-11-03 03:31:14 +0700

    chore: remove import dataUri

--- src/utils/dataUri.ts
+++ src/utils/dataUri.ts
@@ -1,8 +1,8 @@
-import * as DataUriParser from 'datauri/parser';
 import * as path from 'path';
 
 export const getBuffer = (file: Express.Multer.File) => {
-  const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
-  return parser.format(extName, file.buffer);
+  const base64 = file.buffer.toString('base64');
+  const mimeType = file.mimetype || 'application/octet-stream';
+  return `data:${mimeType};base64,${base64}`;
 };
