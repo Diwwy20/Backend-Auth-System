1df3e053757386e0acd3d9348ab95cb10bf4724b
Diwwy20 <123185597+Diwwy20@users.noreply.github.com>
2025-11-03 02:47:59 +0700

    chore: delete parameter cors

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: process.env.CLIENT_URL, 
-    credentials: true, 
-  });
+  app.enableCors();
   
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
